85440f75dd7102d62e4be3be92078355e0e2366b
C0dick <145269352+C0dick@users.noreply.github.com>
2025-05-02 19:46:07 +0300

    Add files via upload

---
+++ MvcLab1/MvcLab1/Controllers/VegetablesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLab1.Controllers
+{
+    public class VegetablesController : Controller
+    {
+        // GET: Vegetables/FirstView
+        public ActionResult FirstView()
+        {
+            List<string> vegetables = new List<string>
+            {
+                "Морковь", "Картофель", "Лук", "Помидор", "Огурец",
+                "Капуста", "Баклажан", "Кабачок", "Свекла", "Редис"
+            };
+            return View(vegetables);
+        }
+
+        // GET: Vegetables/SecondView
+        public ActionResult SecondView()
+        {
+            List<string> vegetables = new List<string>
+            {
+                "Морковь", "Картофель", "Лук", "Помидор", "Огурец",
+                "Капуста", "Баклажан", "Кабачок", "Свекла", "Редис"
+            };
+            vegetables.Sort();
+            return View(vegetables);
+        }
+
+        // GET: Vegetables/ThirdView
+        public ActionResult ThirdView()
+        {
+            List<string> vegetables = new List<string>
+            {
+                "Морковь", "Картофель", "Лук", "Помидор", "Огурец",
+                "Капуста", "Баклажан", "Кабачок", "Свекла", "Редис"
+            };
+            vegetables.Sort();
+            return View(vegetables);
+        }
+    }
+}

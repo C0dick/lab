88f8f40bb31b9b327f76392b165a3c7ce12ce902
C0dick <145269352+C0dick@users.noreply.github.com>
2025-05-02 18:55:37 +0300

    Add files via upload

---
+++ SurveyApp/SurveyApp/Models/ViewModels/PersonVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurveyApp.Models.ViewModels
+{
+    public class PersonVM
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("ФИО")]
+        [Required(ErrorMessage = "Поле ФИО обязательно для заполнения")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "ФИО должно быть от 5 до 100 символов")]
+        public string FullName { get; set; }
+
+        [DisplayName("Возраст")]
+        [Required(ErrorMessage = "Поле Возраст обязательно для заполнения")]
+        [Range(1, 120, ErrorMessage = "Возраст должен быть от 1 до 120 лет")]
+        public int Age { get; set; }
+
+        [DisplayName("Пол")]
+        [Required(ErrorMessage = "Поле Пол обязательно для заполнения")]
+        public string Gender { get; set; }
+
+        [DisplayName("Имеет работу")]
+        public bool HasJob { get; set; }
+    }
+}
